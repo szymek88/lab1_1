09ba32ffe71220b3e443417138da62a3ec392565
Szymon Papuga <szymek.p43@gmail.com>
2016-03-31 01:44:33 +0200

    dodany konstruktor oraz metody equals i hashcode klasy Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -9,6 +9,11 @@ public class Money {
 	public Money() {
 	}
 
+	public Money(BigDecimal value, String currency) {
+		this.value = value;
+		this.currency = currency;
+	}
+
 	public BigDecimal getValue() {
 		return value;
 	}
@@ -23,5 +28,43 @@ public class Money {
 
 	public void setCurrency(String currency) {
 		this.currency = currency;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Money other = (Money) obj;
+		if (currency == null) {
+			if (other.currency != null) {
+				return false;
+			}
+		} else if (!currency.equals(other.currency)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
 	}
 }
