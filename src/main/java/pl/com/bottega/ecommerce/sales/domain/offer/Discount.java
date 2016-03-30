acc0a17903062b6f92211d5b47841d22c0890b37
Szymon Papuga <szymek.p43@gmail.com>
2016-03-31 01:28:07 +0200

    metody equals i hascode dodane dla klasy Discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -26,6 +26,44 @@ public class Discount {
 	public void setDiscount(BigDecimal discount) {
 		this.discount = discount;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((discount == null) ? 0 : discount.hashCode());
+		result = prime * result + ((discountCause == null) ? 0 : discountCause.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Discount other = (Discount) obj;
+		if (discount == null) {
+			if (other.discount != null) {
+				return false;
+			}
+		} else if (!discount.equals(other.discount)) {
+			return false;
+		}
+		if (discountCause == null) {
+			if (other.discountCause != null) {
+				return false;
+			}
+		} else if (!discountCause.equals(other.discountCause)) {
+			return false;
+		}
+		return true;
+	}
 	
 	
 }
